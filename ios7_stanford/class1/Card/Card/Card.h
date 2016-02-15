26c6e6752fa878ff937587e36d02be0061988503
ybdesire <ybdesire@gmail.com>
2016-02-15 18:23:46 +0800

    Init Xcode project for learning Objective-C class 'Card'.

---
+++ ios7_stanford/class1/Card/Card/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Card
+//
+//  Created by biny on 2/15/16.
+//  Copyright © 2016 biny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ ios7_stanford/class1/Card/Card/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Card
+//
+//  Created by biny on 2/15/16.
+//  Copyright © 2016 biny. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
