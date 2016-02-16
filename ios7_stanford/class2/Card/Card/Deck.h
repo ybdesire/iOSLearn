0e9ef57dffbfd1cc7e93eec0937855871559e02a
ybdesire <ybdesire@gmail.com>
2016-02-16 14:21:52 +0800

    Init blank calss Deck.

---
+++ ios7_stanford/class2/Card/Card/Deck.h
@@ -0,0 +1,14 @@
+//
+//  Deck.h
+//  Card
+//
+//  Created by biny on 2/16/16.
+//  Copyright © 2016 biny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Deck : NSObject
+
+@end
---
+++ ios7_stanford/class2/Card/Card/Deck.m
@@ -0,0 +1,22 @@
+//
+//  Deck.m
+//  Card
+//
+//  Created by biny on 2/16/16.
+//  Copyright © 2016 biny. All rights reserved.
+//
+
+#import "Deck.h"
+
+// Deck类私有方法
+@interface Deck()
+
+@end
+
+//----------------------------------------------
+// Deck类实现
+@implementation Deck
+
+
+
+@end
